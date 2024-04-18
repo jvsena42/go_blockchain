380d8be37f1f42cf5938aef3d08865a3ec9a00c2
Joao Victor Sena <jvsena16@gmail.com>
2024-04-18 19:36:47 -0300

    feat: create block chain

--- blockchain.go
+++ blockchain.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -13,7 +14,7 @@ type Block struct {
 	transactions []string
 }
 
-func newBlock(nonce int, previousHash string) *Block {
+func NewBlock(nonce int, previousHash string) *Block {
 	b := new(Block)
 	b.timeStamp = time.Now().UnixNano()
 	b.previousHash = previousHash
@@ -28,11 +29,44 @@ func (b *Block) Print() {
 	fmt.Printf("transactions:\t%s\n", b.transactions)
 }
 
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "hash #0 genesis block")
+	return bc
+}
+
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Block %d %s\n", strings.Repeat("=", 10), i, strings.Repeat("=", 10))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("#", 30))
+}
+
 func init() {
 	log.SetPrefix("Blockchain node: ")
 }
 
 func main() {
-	b := newBlock(0, "first hash")
-	b.Print()
+	blockchain := NewBlockchain()
+
+	blockchain.CreateBlock(89, "hash #1")
+
+	blockchain.CreateBlock(5, "hash #2")
+
+	blockchain.CreateBlock(32, "hash #3")
+
+	blockchain.CreateBlock(44, "hash #4")
+	blockchain.Print()
 }
