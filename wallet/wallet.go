d032bec8091a84c9213c823bc93aa2baf0a31db5
Joao Victor Sena <jvsena16@gmail.com>
2024-04-21 11:56:30 -0300

    feat: format keys to string

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"fmt"
 )
 
 type Wallet struct {
@@ -23,6 +24,14 @@ func (w *Wallet) PrivateKey() *ecdsa.PrivateKey {
 	return w.privateKey
 }
 
+func (w *Wallet) PrivateKeyStr() string {
+	return fmt.Sprintf("%x", w.privateKey.D.Bytes())
+}
+
 func (w *Wallet) PublicKey() *ecdsa.PublicKey {
 	return w.publicKey
+}
+
+func (w *Wallet) PublicKeyStr() string {
+	return fmt.Sprintf("%x%x", w.publicKey.X.Bytes(), w.publicKey.Y.Bytes())
 }
