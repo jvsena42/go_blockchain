4d1da3dac9dae97781c5761e99251c1426ed89dd
Joao Victor Sena <jvsena16@gmail.com>
2024-04-25 20:34:27 -0300

    feat: blockhain node code

---
+++ blockchain_node/blockchain_node.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type BlockchainNode struct {
+	port uint64
+}
+
+func NewBlockchainNode(port uint64) *BlockchainNode {
+	return &BlockchainNode{
+		port: port,
+	}
+}
+
+func (bcn *BlockchainNode) Port() uint64 {
+	return bcn.port
+}
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello blockchain")
+}
+
+func (bcn *BlockchainNode) Run() {
+	http.HandleFunc("/", HelloWorld)
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(bcn.port)), nil))
+}
